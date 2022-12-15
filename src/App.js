82665b22b1e5d6ae2743301c131f3f51de146f4b
James <adeshina413@gmail.com>
2022-12-15 06:36:57 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+    //    const [advice, setAdvice] = React.useState({advice:''});
+       this.state = {
+        advice: ''
+      };
+    }
+    
+
+    componentDidMount(){
+        this.FetchQoute()
+    }
+
+    FetchQoute = () => {
+         fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
+        .then(data => {
+            const {id, advice} = data.slip
+            this.setState({advice: advice})
+        })
+        .catch(err => console.error(err))
+    }
+
+    render(){
+        return(
+            <div className='app'>
+                <div className='card'>
+                    <h1 className='heading'>{this.state.advice}</h1> 
+                    <button className="button"  onClick={this.FetchQoute}>
+                        <span>GIVE ME ADVICE!</span>
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
+
 
 export default App;
